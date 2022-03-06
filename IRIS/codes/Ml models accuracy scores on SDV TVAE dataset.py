5cfc6e589ade8c9ab7ebc5fa979540c59e55d934
Ali Valiyev <37627092+AliValiyev@users.noreply.github.com>
2022-03-07 00:55:08 +0400

    Add files via upload

---
+++ IRIS/codes/Ml models accuracy scores on SDV TVAE dataset.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  7 00:36:54 2022
+
+@author: alivaliyev
+"""
+
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+#Metrics
+from sklearn.metrics import accuracy_score
+
+#Model Select
+from sklearn.model_selection import KFold,train_test_split,cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import  LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import linear_model
+from sklearn.linear_model import SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC
+from sklearn.naive_bayes import GaussianNB
+
+iris=pd.read_csv('model_TVAEforIris.csv')
+#Removing the unneeded column
+iris.drop(columns="Id",inplace=True)
+
+
+#Applying machine learning techniques
+X=iris.iloc[:,0:4].values
+y=iris.iloc[:,4].values
+
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+#Train and Test split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=0)
+
+# Random Forest:
+
+
+random_forest = RandomForestClassifier(n_estimators=100)
+random_forest.fit(X_train, y_train)
+Y_prediction = random_forest.predict(X_test)
+accuracy_rf=round(accuracy_score(y_test,Y_prediction)* 100, 2)
+
+
+#Logistic regression
+
+logreg = LogisticRegression(solver= 'lbfgs',max_iter=400)
+logreg.fit(X_train, y_train)
+Y_pred = logreg.predict(X_test)
+accuracy_lr=round(accuracy_score(y_test,Y_pred)* 100, 2)
+
+
+# K Nearest Neighbor
+
+knn = KNeighborsClassifier(n_neighbors = 3)
+knn.fit(X_train, y_train)
+Y_pred = knn.predict(X_test) 
+accuracy_knn=round(accuracy_score(y_test,Y_pred)* 100, 2)
+
+
+
+#Naive Bayes
+
+gaussian = GaussianNB()
+gaussian.fit(X_train, y_train)
+Y_pred = gaussian.predict(X_test) 
+accuracy_nb=round(accuracy_score(y_test,Y_pred)* 100, 2)
+
+
+#SVM
+
+linear_svc = LinearSVC(max_iter=4000)
+linear_svc.fit(X_train, y_train)
+Y_pred = linear_svc.predict(X_test)
+accuracy_svc=round(accuracy_score(y_test,Y_pred)* 100, 2)
+
+
+#accuracy table
+
+results = pd.DataFrame({
+    'Model': [ 'KNN', 
+              'Logistic Regression', 
+              'Random Forest',
+              'Naive Bayes',  
+              ' Support Vector Machine'],
+    "Accuracy_score":[accuracy_knn,
+                      accuracy_lr,
+                      accuracy_rf,
+                      accuracy_nb,
+                      accuracy_svc
+                     ]})
+result_df = results.sort_values(by='Accuracy_score', ascending=False)
+result_df = result_df.reset_index(drop=True)
+result_df.head(9)
